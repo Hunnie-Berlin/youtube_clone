c61f307f3b690c6de6d728e462c0a79d9e72cd4c
CHEOL HUN LEE <doulos2008@naver.cpm>
2020-09-21 11:00:29 +0200

    add variables to pug
    pageTitle and siteName

--- app.js
+++ app.js
@@ -4,18 +4,21 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import { localsMiddleware } from "./middlewares";
 import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 
 const app = express();
 
-app.set("view engine", "pug");
 app.use(helmet());
+app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
+
+app.use(localsMiddleware);
 
 app.use("/", globalRouter);
 app.use("/users", userRouter);
