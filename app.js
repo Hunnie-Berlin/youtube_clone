b7d4c654454c6ee1c78230218aed49f38c49151b
CHEOL HUN LEE <doulos2008@naver.cpm>
2020-09-24 11:00:37 +0200

    Controller: home
    with fake db

--- app.js
+++ app.js
@@ -18,6 +18,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 
+app.use(function (req, res, next) {
+  res.setHeader(
+    "Content-Security-Policy",
+    "script-src 'self' https://archive.org"
+  );
+  return next();
+});
+// Temporally for test
+
 app.use(localsMiddleware);
 
 app.use("/", globalRouter);
---
+++ db.js
@@ -0,0 +1,53 @@
+export const videos = [
+  {
+    id: 3123123,
+    title: "Video Awesome",
+    description: "This is something I like1",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 121212,
+      name: "Nico",
+      email: "itnico@las.com",
+    },
+  },
+  {
+    id: 56456546,
+    title: "Video Nice",
+    description: "This is something I like2",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 239048234,
+      name: "Nico",
+      email: "itnico@las.com",
+    },
+  },
+  {
+    id: 39834234,
+    title: "Video Good",
+    description: "This is something I like3",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 121212,
+      name: "Nico",
+      email: "itnico@las.com",
+    },
+  },
+  {
+    id: 1498734,
+    title: "Video Super",
+    description: "This is something I like4",
+    views: 24,
+    videoFile: "",
+    creator: {
+      id: 121212,
+      name: "Nico",
+      email: "itnico@las.com",
+    },
+  },
+];
