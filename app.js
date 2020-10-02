22bec2104c26d88903bcfd1a862ce092b08ec03f
CHEOL HUN LEE <doulos2008@naver.cpm>
2020-10-02 09:54:04 +0200

    webpack setting: scss to css

--- app.js
+++ app.js
@@ -11,22 +11,14 @@ import videoRouter from "./routers/videoRouter";
 
 const app = express();
 
-app.use(helmet());
+app.use(helmet({ contentSecurityPolicy: false }));
 app.set("view engine", "pug");
 app.use("/uploads", express.static("uploads"));
+app.use("/static", express.static("static"));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
-
-app.use(function (req, res, next) {
-  res.setHeader(
-    "Content-Security-Policy",
-    "script-src 'self' https://archive.org"
-  );
-  return next();
-});
-// Temporally for test
 
 app.use(localsMiddleware);
 
