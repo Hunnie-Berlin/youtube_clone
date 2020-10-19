28edc578d00f72b222553db040064dc7c3dd916b
CHEOL HUN LEE <doulos2008@naver.cpm>
2020-10-19 11:02:35 +0200

    volume bar

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -5,6 +5,7 @@ const volumeBtn = document.getElementById("jsVolumeBtn");
 const fullScreen = document.getElementById("jsFullScreen");
 const currentTime = document.getElementById("currentTime");
 const totalTime = document.getElementById("totalTime");
+const volumeRange = document.getElementById("jsVolume");
 
 
 function handlePlayClick() {
@@ -21,9 +22,11 @@ function handleVolumeClick() {
     if (videoPlayer.muted) {
         videoPlayer.muted = false;
         volumeBtn.innerHTML = '<i class="fas fa-volume-up"></i>';
+        volumeRange.value = videoPlayer.volume;
     } else {
         videoPlayer.muted = true;
         volumeBtn.innerHTML = '<i class="fas fa-volume-mute"></i>';
+        volumeRange.value = 0;
     }
 }
 
@@ -88,11 +91,35 @@ function setTotalTime() {
   videoPlayer.addEventListener("timeupdate", getCurrentTime);
 }
 
+function handleEnded() {
+    videoPlayer.currentTime = 0;
+    playBtn.innerHTML = '<i class="fas fa-play"></i>'
+}
+
+function handleDrag(event) {
+    const {target: {value}} = event;
+    videoPlayer.volume = value;
+    if (videoPlayer.muted){
+        videoPlayer.muted = false;
+    }
+    if (value >= 0.6) {
+        volumeBtn.innerHTML = '<i class="fas fa-volume-up"></i>';
+      } else if (value >= 0.2) {
+        volumeBtn.innerHTML = '<i class="fas fa-volume-down"></i>';
+      } else {
+        volumeBtn.innerHTML = '<i class="fas fa-volume-off"></i>';
+      }
+    }
+  
+
 function init(){
+    videoPlayer.volume = 0.5;
     playBtn.addEventListener("click", handlePlayClick);
     volumeBtn.addEventListener("click", handleVolumeClick);
     fullScreen.addEventListener("click", goFullScreen);
     videoPlayer.addEventListener("loadedmetadata", setTotalTime);
+    videoPlayer.addEventListener("ended", handleEnded)
+    volumeRange.addEventListener("input", handleDrag);
 }
 
 if (videoContainer) {
