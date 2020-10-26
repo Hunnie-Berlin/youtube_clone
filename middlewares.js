cb459aa0ca728d805928cfaa9563acb157d2375a
CHEOL HUN LEE <doulos2008@naver.cpm>
2020-10-26 14:03:13 +0100

    fix tiny thing

--- middlewares.js
+++ middlewares.js
@@ -2,18 +2,17 @@ import multer from "multer";
 import routes from "./routes";
 
 const multerVideo = multer({ dest: "uploads/videos/" });
-const multerAvatar = multer({dest: "uploads/avatars/"});
+const multerAvatar = multer({ dest: "uploads/avatars/" });
 
 export const localsMiddleware = (req, res, next) => {
   res.locals.siteName = "Metube";
   res.locals.routes = routes;
   res.locals.loggedUser = req.user || null;
-  console.log(req.user);
   next();
 };
 
-export const onlyPublic = (req, res, next) =>{
-  if(req.user){
+export const onlyPublic = (req, res, next) => {
+  if (req.user) {
     res.redirect(routes.home);
   } else {
     next();
@@ -21,7 +20,7 @@ export const onlyPublic = (req, res, next) =>{
 };
 
 export const onlyPrivate = (req, res, next) => {
-  if (req.user){
+  if (req.user) {
     next();
   } else {
     res.redirect(routes.home);
