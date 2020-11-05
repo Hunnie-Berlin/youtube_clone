b8201185aa4ad5b9940d1d6dccc6b632c58e6ffd
CHEOL HUN LEE <doulos2008@naver.cpm>
2020-11-05 21:19:06 +0100

    production url option tp DB, passport github oauth

--- src/passport.js
+++ src/passport.js
@@ -2,15 +2,24 @@ import passport from "passport";
 import GithubStrategy from "passport-github2";
 import { githubLoginCallback } from "./controllers/userController";
 import User from "./models/User";
+import dotenv from "dotenv";
+dotenv.config();
 
 passport.use(User.createStrategy());
 
-passport.use(new GithubStrategy({
-    clientID: process.env.GH_ID,
-    clientSecret: process.env.GH_SECRET,
-    callbackURL: "http://localhost:4000/auth/github/callback",
-    scope: ["user:email"],
-}, githubLoginCallback));
+passport.use(
+  new GithubStrategy(
+    {
+      clientID: process.env.GH_ID,
+      clientSecret: process.env.GH_SECRET,
+      callbackURL: process.env.PRODUCTION
+        ? "https://hidden-bayou-14330.herokuapp.com/auth/github/callback"
+        : "http://localhost:4000/auth/github/callback",
+      scope: ["user:email"],
+    },
+    githubLoginCallback
+  )
+);
 
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
